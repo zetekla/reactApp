5ffaf69f7360536b523890155e44f03d84a71c19
Kallio <pristinekallio@gmail.com>
2016-11-26 10:50:15 -0800

    passing App:state as data source to child components, they read props.

--- App.jsx
+++ App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 export default class App extends React.Component {
-	constructor(){
-		super();
+	constructor(props){
+		super(props);
 
 		this.state = {
-       header: "Header from state...",
-       "content": "Content from state..."
+       header: "Header from props...",
+       "content": "Content from props..."
     }
 	}
 
@@ -30,22 +30,30 @@ export default class App extends React.Component {
 						<span>{int == 1 ? 'True!' : 'False'}</span>
 						<h1 style = {Olive}>Main page</h1>
 						<h1 style = {myStyle}>{this.props.immutableData}</h1>
-						<h1>{this.state.header}</h1>
-            <h2>{this.state.content}</h2>
+						<Header headerProp = {this.state.header}/>
+            <Content contentProp = {this.state.content}/>
          </div>
 
       );
    }
 }
 
-class TableRow extends React.Component {
-	render() {
-		return (
-			<tr>
-				<td>{this.props.data.id}</td>
-				<td>{this.props.data.name}</td>
-				<td>{this.props.data.age}</td>
-			</tr>
-		);
-	}
+class Header extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>{this.props.headerProp}</h1>
+         </div>
+      );
+   }
+}
+
+class Content extends React.Component {
+   render() {
+      return (
+         <div>
+            <h2>{this.props.contentProp}</h2>
+         </div>
+      );
+   }
 }
