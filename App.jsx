cb035b8ccec3a816715900f4a9f0260b2ca44426
Kallio <pristinekallio@gmail.com>
2016-11-27 21:30:18 -0800

    Component Life Cycle, unmount gone by 8-secs defined in main.js

--- App.jsx
+++ App.jsx
@@ -7,61 +7,79 @@ export default class App extends React.Component {
 		super(props);
 
 		this.state = {
-		  data: []
+		  data: 0
 		}
 
-    this.setStateHandler 		= this.setStateHandler.bind(this);
-    this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
-    this.findDomNodeHandler = this.findDomNodeHandler.bind(this);
+		this.setNewNumber = this.setNewNumber.bind(this)
 	}
 
-	setStateHandler() {
-	  var item = "setState..."
-	  var myArray = this.state.data;
-	  myArray.push(item)
-	  this.setState({data: myArray})
-	};
-
-	forceUpdateHandler() {
-	  this.forceUpdate();
-	};
-
-	findDomNodeHandler() {
-    var myDiv = document.getElementById('myDiv');
-    ReactDOM.findDOMNode(myDiv).style.color = 'green';
+	setNewNumber() {
+      this.setState({data: this.state.data + 1})
   }
 
   render() {
     return (
-       <div>
-          <button onClick = {this.setStateHandler}>SET STATE</button>
-          <h4>State Array: {this.state.data}</h4>
-					<br/>
-          <button onClick = {this.forceUpdateHandler}>FORCE UPDATE</button>
-          <h4>Random number: {Math.random()}</h4>
-					<br/>
-					<button onClick = {this.findDomNodeHandler}>FIND DOM NODE</button>
-          <div id = "myDiv">NODE</div>
-       </div>
+			<div>
+			  <button onClick   = {this.setNewNumber}>INCREMENT</button>
+			  <Content myNumber = {this.state.data}></Content>
+			</div>
     );
   }
 }
 
-class Header extends React.Component {
-   render() {
-      return (
-         <div>
-            <h1>{this.props.headerProp}</h1>
-         </div>
-      );
-   }
-}
-
 class Content extends React.Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+		  value: 0
+		}
+	}
+
+   componentWillMount() {
+   		this.state.value++;
+      console.log('Component WILL MOUNT!', this.state.value)
+   }
+
+   componentDidMount() {
+   		this.state.value++;
+      console.log('Component DID MOUNT!', this.state.value)
+   }
+
+   componentWillReceiveProps(newProps) {
+   		this.state.value++;
+      console.log('Component WILL RECIEVE PROPS!', this.state.value)
+   }
+
+   shouldComponentUpdate(newProps, newState) {
+   		this.state.value++;
+      return true;
+   }
+
+   componentWillUpdate(nextProps, nextState) {
+   		this.state.value++;
+ 			console.log('Component WILL UPDATE!', this.state.value);
+   }
+
+   componentDidUpdate(prevProps, prevState) {
+   		this.state.value++;
+      console.log('Component DID UPDATE!', this.state.value)
+   }
+
+   componentWillUnmount() {
+   		this.state.value++;
+      console.log('Component WILL UNMOUNT! gone by 8-secs', this.state.value)
+   }
+
    render() {
+			var Green = {
+			  fontSize: 50,
+			  color: 'Green'
+			}
       return (
          <div>
-            <h2>{this.props.contentProp}</h2>
+            <h3>{this.props.myNumber}</h3>
+            <h3 style={Green}>{this.state.value}</h3>
          </div>
       );
    }
--- main.js
+++ main.js
@@ -4,3 +4,6 @@ import App from './App.jsx';
 // import PropsValidation from './PropsValidation.jsx';
 
 ReactDOM.render(<App immutableData ="look, I'm a props and I'm invincible!"/>, document.getElementById('app'));
+
+setTimeout(() => {
+   ReactDOM.unmountComponentAtNode(document.getElementById('app'));}, 8000);
