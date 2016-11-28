620b02e80ca5d17ad08675edafaee265abbe2b1d
Kallio <pristinekallio@gmail.com>
2016-11-27 21:44:55 -0800

    Binding (Forms) in ReactJS

--- App.jsx
+++ App.jsx
@@ -3,84 +3,47 @@ import ReactDOM from 'react-dom';
 // import PropsValidation from './PropsValidation.jsx';
 
 export default class App extends React.Component {
-	constructor(props){
-		super(props);
+	constructor(props) {
+      super(props);
 
-		this.state = {
-		  data: 0
-		}
+      this.state = {
+         data: 'Initial data...',
+         invincible: this.props.immutableData
+      }
 
-		this.setNewNumber = this.setNewNumber.bind(this)
-	}
+      this.updateState = this.updateState.bind(this);
 
-	setNewNumber() {
-      this.setState({data: this.state.data + 1})
-  }
+   };
 
-  render() {
-    return (
-			<div>
-			  <button onClick   = {this.setNewNumber}>INCREMENT</button>
-			  <Content myNumber = {this.state.data}></Content>
-			</div>
-    );
-  }
-}
-
-class Content extends React.Component {
-	constructor(props){
-		super(props);
-
-		this.state = {
-		  value: 0
-		}
-	}
-
-   componentWillMount() {
-   		this.state.value++;
-      console.log('Component WILL MOUNT!', this.state.value)
-   }
-
-   componentDidMount() {
-   		this.state.value++;
-      console.log('Component DID MOUNT!', this.state.value)
-   }
-
-   componentWillReceiveProps(newProps) {
-   		this.state.value++;
-      console.log('Component WILL RECIEVE PROPS!', this.state.value)
-   }
-
-   shouldComponentUpdate(newProps, newState) {
-   		this.state.value++;
-      return true;
-   }
-
-   componentWillUpdate(nextProps, nextState) {
-   		this.state.value++;
- 			console.log('Component WILL UPDATE!', this.state.value);
-   }
-
-   componentDidUpdate(prevProps, prevState) {
-   		this.state.value++;
-      console.log('Component DID UPDATE!', this.state.value)
-   }
-
-   componentWillUnmount() {
-   		this.state.value++;
-      console.log('Component WILL UNMOUNT! gone by 8-secs', this.state.value)
+   updateState(e) {
+      this.setState({data: e.target.value});
    }
 
    render() {
-			var Green = {
-			  fontSize: 50,
-			  color: 'Green'
-			}
       return (
          <div>
-            <h3>{this.props.myNumber}</h3>
-            <h3 style={Green}>{this.state.value}</h3>
+            <input type = "text" value = {this.state.data}
+               onChange = {this.updateState} />
+            <h4>{this.state.data}</h4>
+            <span style={{color:'Red'}}>{this.state.invincible}</span>
+            <br/>
+            <Content myDataProp = {this.state.data}
+               updateStateProp = {this.updateState}></Content>
          </div>
       );
    }
+}
+
+class Content extends React.Component {
+	render() {
+	  return (
+	     <div>
+	     		<br/>
+	     		<span style={{color:'olive'}}>FROM the Content Component</span>
+	        <input type = "text" value = {this.props.myDataProp}
+	           onChange = {this.props.updateStateProp} />
+	        <h3>{this.props.myDataProp}</h3>
+	     </div>
+	  );
+	}
 }
--- main.js
+++ main.js
@@ -4,6 +4,3 @@ import App from './App.jsx';
 // import PropsValidation from './PropsValidation.jsx';
 
 ReactDOM.render(<App immutableData ="look, I'm a props and I'm invincible!"/>, document.getElementById('app'));
-
-setTimeout(() => {
-   ReactDOM.unmountComponentAtNode(document.getElementById('app'));}, 8000);
