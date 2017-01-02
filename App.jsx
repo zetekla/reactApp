55bbac8794cf2bfca76d1856211419bfd66cf873
Kallio <pristinekallio@gmail.com>
2017-01-01 22:44:12 -0800

    r1 import and export dependent classes within App.jsx

--- App.jsx
+++ App.jsx
@@ -1,14 +1,26 @@
 import React      from 'react';
 import {Link} from 'react-router';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
     return(
       <div>
       <ul>
-      <li><Link to="home">Home</Link></li>
-      <li><Link to="about">About</Link></li>
-      <li><Link to="contact">Contact</Link></li>
+         <li>
+            <Link to="/">Main</Link>
+         </li>
+         <li>
+            <Link to="home">Home</Link>
+         </li>
+         <li>
+            <Link to="about">About</Link>
+         </li>
+         <li>
+            <Link to="contact">Contact</Link>
+         </li>
       </ul>
 
       {this.props.children}
@@ -17,28 +29,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
-
-export class Home extends React.Component {
-  render() {
-    return (
-      <h1>Home Page Content</h1>
-    )
-  }
-}
-
-export class About extends React.Component {
-  render() {
-    return (
-      <h1>About Page Content</h1>
-    )
-  }
-}
-
-export class Contact extends React.Component {
-  render()  {
-    return (
-      <h1>Contact Page Content</h1>
-    )
-  }
-}
+export {Home, About, Contact}
