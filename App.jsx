d31981adbb4a98e7017ac72492468fb4dacd688a
Pristine Kallio <pristinekallio@gmail.com>
2017-03-12 22:07:48 -0700

    App.jsx and main.js

--- App.jsx
+++ App.jsx
@@ -1,21 +1,46 @@
-import React      from 'react';
-import {Link}     from 'react-router';
-import Home       from './components/Home';
-import About      from './components/About';
-import Contact    from './components/Contact';
-import Nav        from './components/Nav';
+import React from 'react';
+import {Link} from 'react-router';
+
 
 export default class App extends React.Component {
-
-
   render() {
     return(
       <div>
-				<Nav />
-				{this.props.children}
+      <ul>
+      <li><Link to="home">Home</Link></li>
+      <li><Link to="about">About</Link></li>
+      <li><Link to="contact">Contact</Link></li>
+      </ul>
+
+      {this.props.children}
       </div>
     )
   }
 }
 
-export {Home, About, Contact}
+
+export class Home extends React.Component {
+  render() {
+    return (
+      <h1>Home...</h1>
+    )
+  }
+}
+
+
+export class About extends React.Component {
+  render() {
+    return (
+      <h1>About...</h1>
+    )
+  }
+}
+
+
+export class Contact extends React.Component {
+  render()  {
+    return (
+      <h1>Contact...</h1>
+    )
+  }
+}
