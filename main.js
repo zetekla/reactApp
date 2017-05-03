9c254ab2932d1f7ed8a54a2fcff41ace92868bb7
Pristine Tran <pristinetran@tycoint.com>
2017-05-03 16:47:25 -0700

    polish

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute  } from 'react-router';
 
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
