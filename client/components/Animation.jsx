b4d021bf7cb4fc2a71885f4d0fb20e5164d14e5d
Pristine Tran <pristinekallio@gmail.com>
2017-08-23 00:18:38 -0700

    simplified Animation

--- client/components/Animation.jsx
+++ client/components/Animation.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+import '../styles/style.css';
 
 export default class Animation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: ['Item 1...', 'Item 2...', 'Item 3...', 'Item 4...']
+    };
+
+    this.handleAdd = this.handleAdd.bind(this);
+  };
+
+  handleAdd() {
+    var newItems = this.state.items.concat([prompt('Create New Item')]);
+    this.setState({items: newItems});
+  }
+
+  handleRemove(i) {
+    var newItems = this.state.items.slice();
+    newItems.splice(i, 1);
+    this.setState({items: newItems});
+  }
+
   render() {
+    var items = this.state.items.map(function (item, i) {
+      return (
+        <div key={item} onClick={this.handleRemove.bind(this, i)}>
+          {item}
+        </div>
+      );
+
+    }.bind(this));
+
     return (
-      <div>
-        <h1>Animation...</h1>
+      <div className="container">
+        <button onClick={this.handleAdd}>Add Item</button>
+        <ReactCSSTransitionGroup transitionName="example"
+                                 transitionEnterTimeout={500} transitionLeaveTimeout={500}>
+          {items}
+        </ReactCSSTransitionGroup>
+        {this.props.children}
       </div>
     )
   }
